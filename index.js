01968d7346e8c32fb4e65f68a80691a38b03aaaa
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-01-06 16:04:29 +0200

    cors change back

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bcrypt = require('bcryptjs'); // Password hash crypt.
 let port = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors({origin: 'https://tatukristiani.github.io'}));
+app.use(cors());
 app.use(express.json());
 
 var bodyParser = require('body-parser');
