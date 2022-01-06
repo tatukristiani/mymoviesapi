e796226a507443397e589916858937583f4307a7
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-01-06 17:06:57 +0200

    appAll add

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ client.connect()
 
 
 
+app.all('/', function(req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+
 app.get("/", (req,res) => {
     res.send("Hello World");
 })
