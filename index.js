bc6e9b4408794e0d59a3e14701358dd4c22e42d7
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-01-06 16:28:21 +0200

    update cors

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ let port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
-app.use(function (req,res,next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
 app.use(express.json());
 
 var bodyParser = require('body-parser');
