3ae60903a083bb7a672a8a8cf503ff812a5a6ec0
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-02-11 00:41:01 +0200

    new test2

--- movieAPI/request.js
+++ movieAPI/request.js
@@ -1,5 +1,5 @@
 const API_KEY  = "ac4ead2bbde49f3cb342413c09f6d25a";
-const BASE_URL = "https://api.themoviedb.org/3"
+const BASE_URL = `https://api.themoviedb.org/3`
 
 const requests = {
     fetchTrending: BASE_URL + `/trending/movie/week?api_key=${API_KEY}&language=en-US`,
@@ -9,7 +9,7 @@ const requests = {
     fetchHorrorMovies: BASE_URL + `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: BASE_URL + `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: BASE_URL + `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchPoster: "https://image.tmdb.org/t/p/original/"
+    fetchPoster: `https://image.tmdb.org/t/p/original/`
 }
 
 module.exports = requests;
