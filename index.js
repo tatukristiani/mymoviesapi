5409ef47f12bdb26e405199089a089b3099664f7
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-02-10 22:48:16 +0200

    Testing fetch movies 2

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ let port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
+/*
 app.use(function(req,res,next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 })
+ */
 
 var bodyParser = require('body-parser');
 import axios from './movieAPI/axios';
