beb05addb90e79c27d9bc4f008e671427f2fa8d1
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-02-10 23:52:49 +0200

    test commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import requests from "./movieAPI/request";
 const jwt = require('jsonwebtoken');
 const secret = "fOzHnFjg0FmM6O/dTVXd/4sGqxgkBdcNwNp00J+QYxm6WljQui0i1Uwk0yp70fQEVIVKNUqM8vYqYgUDWeO0w/GsjgH0QuaoyfbSoHWLrrrIFwIvQR7V7zm535HaOnHzC6QmKElDneqU1MMGPFDxepGD5TaRZ+uGVdhYg26s/azEngpf+FKNJTZYAXebx/ByAmdVhIuVIRok0NJLLZZe/njZOh7jBdcOJZq7GBedTASSdpK7CgKtplE8PwGQ8QrPhiW5besygWKuoDF90ap591+/vN1lMCEam6KfBPxi9D1GTjUMe5cjgpz34NvqP9+sXns+UkejzY5tqBdstl64VQ==";
 
-require('dotenv').config();
+const env = require('dotenv').config();
 const url = require('url');
 const util = require('util');
 const express = require('express');
