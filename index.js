65117ca5d7be3d424f4a98e3416d8ed022d807fd
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-02-18 20:51:33 +0200

    testing if adding to database works

--- index.js
+++ index.js
@@ -260,8 +260,8 @@ app.post('/api/movies', function(req,res) {
                 // Search for movies ID
                 sql = `SELECT id FROM movie WHERE title=` + `'` + title + `'` + ` AND tmdbid = ` + tmdbID;
                 results = await client.query(sql);
-                let movieID = results.rows[0];
-                res.send(movieID);
+                let movieID = results.rows[0].id;
+                res.send("id" + movieID);
 
 
                 /*
