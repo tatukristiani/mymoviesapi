dad13bc0f22002a63c37315a6e567e9ca3d9acb3
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-02-19 05:41:41 +0200

    testing if adding to database works

--- index.js
+++ index.js
@@ -142,6 +142,7 @@ app.post('/api/movies', function(req,res) {
         // Check if the movie is in database. If it's not add it there.
         (async () => {
             try {
+                let sql;
                 let results = await client.query('SELECT id, title FROM movie WHERE title=$1$ AND tmdbid=$2$', [title, tmdbID]);
                 /*
                 // Check if the movie is already in the database.
@@ -166,8 +167,9 @@ app.post('/api/movies', function(req,res) {
                 let userID = results.rows[0].id;
 
                 // Search for movies ID
-                sql = `SELECT id FROM movie WHERE title=` + `'` + title + `'` + ` AND tmdbid = ` + tmdbID;
-                results = await client.query(sql);
+                results = await client.query('SELECT id FROM movie WHERE title=$1$ AND tmdbid=$2$', [title, tmdbID]);
+               // sql = `SELECT id FROM movie WHERE title=` + `'` + title + `'` + ` AND tmdbid = ` + tmdbID;
+                //results = await client.query(sql);
                 let movieID = results.rows[0].id;
 
 
