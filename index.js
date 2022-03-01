53ce000620731eb83297c69944636a9f0c1ddd14
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-03-01 19:06:21 +0200

    Test register

--- index.js
+++ index.js
@@ -74,14 +74,20 @@ app.post('/api/users', function(req,res){
     let username = data.username;
     let email = data.email;
 
-    (async () => {
-        try {
-            await client.query(`UPDATE users SET username=$1, email=$2 WHERE username=$3`, [newUsername,email,username]);
-            res.status(200).json({"message": "Successfully updated user credentials!"})
-        }catch (err) {
-            res.status(500).json({"message": "Error while trying to update user credentials."});
-        }
-    })();
+    if(validateEmail(email) && validateCredential(newUsername)) {
+        (async () => {
+            try {
+                await client.query(`UPDATE users SET username=$1, email=$2 WHERE username=$3`, [newUsername, email, username]);
+                res.status(200).json({"message": "Successfully updated user credentials!"})
+            } catch (err) {
+                res.status(500).json({"message": "Error while trying to update user credentials."});
+            }
+        })();
+    } else if(validateEmail(email)){
+        res.status(400).json({"message":"Username must be 4-20 characters & can't start or end with a . or _ or have two of them in a row!"});
+    } else {
+        res.status(400).json({"message":"Email address must be a valid email! i.e. example@email.com"});
+    }
 
 })
 
