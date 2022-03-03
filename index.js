d877ab5c457376d134c41e66b04b3f9cabb00cc6
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2022-03-03 15:47:41 +0200

    test swagger

--- index.js
+++ index.js
@@ -114,15 +114,15 @@ client.connect()
  *           type: number
  *       example:
  *         adult: false
- *         backdrop_path: /iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg
+ *         backdrop_path: "/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg"
  *         id: 634649
- *         original_language: en
- *         original_title: Spider-Man: No Way Home
- *         overview: Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.
+ *         original_language: "en"
+ *         original_title: "Spider-Man: No Way Home"
+ *         overview: "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man."
  *         popularity:  5596.919
- *         poster_path: /1g0dhYtq4irTY1GPXvft6k4YLjm.jpg
- *         release_date: 2021-12-15
- *         title: Spider-Man: No Way Home
+ *         poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+ *         release_date: "2021-12-15"
+ *         title: "Spider-Man: No Way Home"
  *         video:  false
  *         vote_average: 8.3
  *         vote_count: 8613
